7b5a5a5fe04e7ba1c264a7974a977682a720f0f5
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-07-30 16:07:52 -0700

    Created + Completed RegisterEmployeeHandler file (with notes). Updated App with RegisterEmployee Handler and path.

--- src/main/java/dev/evanishyn/app/App.java
+++ src/main/java/dev/evanishyn/app/App.java
@@ -1,4 +1,30 @@
 package dev.evanishyn.app;
 
+import dev.evanishyn.daos.EmployeeDAO;
+import dev.evanishyn.daos.EmployeeDAOLocal;
+import dev.evanishyn.entities.Employee;
+import dev.evanishyn.handlers.RegisterEmployeeHandler;
+import dev.evanishyn.services.EmployeeService;
+import dev.evanishyn.services.EmployeeServiceImpl;
+import io.javalin.Javalin;
+
 public class App {
+
+    public static EmployeeService employeeService = new EmployeeServiceImpl(new EmployeeDAOLocal());
+
+    public static void main(String[] args) {
+        Javalin app = Javalin.create();
+
+
+        RegisterEmployeeHandler registerEmployeeHandler = new RegisterEmployeeHandler();
+
+
+        app.post("/register-employee", registerEmployeeHandler);
+
+
+
+
+        app.start();
+    }
+
 }
---
+++ src/main/java/dev/evanishyn/handlers/RegisterEmployeeHandler.java
@@ -0,0 +1,33 @@
+package dev.evanishyn.handlers;
+
+import com.google.gson.Gson;
+import dev.evanishyn.app.App;
+import dev.evanishyn.entities.Employee;
+import io.javalin.http.Context;
+import io.javalin.http.Handler;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class RegisterEmployeeHandler implements Handler {
+
+    @Override
+    public void handle(@NotNull Context ctx) throws Exception{
+        String json = ctx.body();       //creates json object and fills with body(?)
+        Gson gson = new Gson();         //Instantiate a gson object (converts to json)
+        Employee employee = gson.fromJson(json, Employee.class);    //create employee object; json -> gson
+        Employee registeredEmployee = App.employeeService.registerEmployeeAccount(employee);    //create registered employee from app
+        String employeeJson = gson.toJson(registeredEmployee);  //create json string from regEmp info
+        ctx.status(201);        //checking for a 201 status
+        ctx.result(employeeJson);       //returning employee Json string
+
+    }
+
+//    public boolean containsIllegalSymbol(Employee employee){
+//        Pattern pattern = Pattern.compile("[~#*+%{}<>\\[\\]|\"\\_^]");
+//        Matcher matcher = pattern.matcher(employee.getEmail());
+//        return matcher.find();
+//    }
+
+}
