75d779d38d1d61ee3b5f72c32295fbcaa0e45726
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-08-01 20:03:34 -0700

    Filled out Expense entity

--- src/main/java/dev/evanishyn/entities/Expense.java
+++ src/main/java/dev/evanishyn/entities/Expense.java
@@ -1,5 +1,8 @@
 package dev.evanishyn.entities;
 
+import dev.evanishyn.utilities.Status;
+import dev.evanishyn.utilities.Type;
+
 public class Expense {
 
     private int id;
@@ -9,5 +12,72 @@ public class Expense {
     private Type type;  //expense.getType().name()   (.name == toString() for enums)
     private Status status;
 
+    public Expense(int id, Employee employee, double amount, String description, Type type, Status status) {
+        this.id = id;
+        this.employee = employee;
+        this.amount = amount;
+        this.description = description;
+        this.type = type;
+        this.status = status;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public void setType(Type type) {
+        this.type = type;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "Expense{" +
+                "id=" + id +
+                ", employee=" + employee +
+                ", amount=" + amount +
+                ", description='" + description + '\'' +
+                ", type=" + type +
+                ", status=" + status +
+                '}';
+    }
 }
