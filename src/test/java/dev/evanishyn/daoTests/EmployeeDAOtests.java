20423b355912fcfe7a7d55ece5a0892849f7ee52
caitevanish <96436738+caitevanish@users.noreply.github.com>
2022-08-03 14:26:50 -0700

    Created Employee DAO tests

--- src/test/java/dev/evanishyn/daoTests/EmployeeDAOtests.java
+++ src/test/java/dev/evanishyn/daoTests/EmployeeDAOtests.java
@@ -5,15 +5,18 @@ import dev.evanishyn.daos.employee_daos.EmployeeDAOLocal;
 import dev.evanishyn.entities.Employee;
 import org.junit.jupiter.api.*;
 
+import java.util.Map;
+
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 
 public class EmployeeDAOtests {
 
-    EmployeeDAO employeeDao = new EmployeeDAOLocal();
+    static EmployeeDAO employeeDao = new EmployeeDAOLocal();
 
     //Post
-    @Test
+    @Test   //Passed
+    @Order(1)
     void create_employee_test(){    //PASSED
         Employee employee = new Employee(0, "Caitlin", "Evanishyn");
         Employee savedEmployee = employeeDao.createEmployee(employee);
@@ -21,9 +24,32 @@ public class EmployeeDAOtests {
     }
 
     //Get
+    @Test   //Passed
+    @Order(2)
+    void get_all_employees_test(){
+        Employee employee1 = new Employee(0, "Moira", "Rose");
+        Employee employee2 = new Employee(0, "David", "Rose");
+        Employee employee3 = new Employee(0, "Johnny", "Rose");
+
+        employeeDao.createEmployee(employee1);
+        employeeDao.createEmployee(employee2);
+        employeeDao.createEmployee(employee3);
+
+        Map<Integer, Employee> employeeList = employeeDao.getEmployeeList();
+        Assertions.assertEquals(4,employeeList.size());
+    }
+
+    @Test   //Passed
+    @Order(3)
+    void get_employee_by_id(){
+        Employee employee = employeeDao.getEmployeeById(1);
+        Assertions.assertEquals("Caitlin", employee.getfName());
+
+    }
 
     //Put
-    @Test
+    @Test   //Passed
+    @Order(4)
     void update_employee_test(){    //PASSED
         Employee employee = new Employee(1, "Daniel", "Troup");
         employeeDao.updateEmployeeInfo(employee);
@@ -32,6 +58,12 @@ public class EmployeeDAOtests {
     }
 
     //Delete
+    @Test
+    @Order(5)
+    void delete_employee_test(){
+        boolean result = employeeDao.deleteEmployeeAccount(1);
+        Assertions.assertTrue(result);
+    }
 
 
 }
